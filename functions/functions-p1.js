652f0acf422d87b05e1dd3b63505b6a1bc61abd9
widyan97 <73026713+widyan97@users.noreply.github.com>
2020-10-27 17:51:09 +0200

    Add files via upload

---
+++ functions/functions-p1.js
@@ -0,0 +1,56 @@
+//ex1
+
+const calcAge = function(year1,year2){
+ return year1-year2
+ 
+    }
+    const age = calcAge(2017, 1989)
+    console.log(age) //returns 28
+
+
+    //ex2
+         const Age = function(year2,year1){
+          age1=year2-year1
+         return age1
+          
+          
+           }
+        const ages = calcAge(2018, 2015)
+           console.log("You are either"+ (ages) +" or "+(ages-1) ) 
+
+        //ex3
+           const isEven = function(num){
+               if (num%2==0)
+               return true
+               else return false}
+
+               const number=isEven(10)
+               console.log(number)
+            
+
+               //ex4
+               const printEven = function(arr){
+                for (let i=0; i<arr.length; i++) {
+
+               let  tf=isEven(numbers)
+                 if (tf==false)
+                   return arr[i]
+
+               }
+            }
+               const numbers = [1, 5, 3,10,15]
+               const nums = printEven(numbers)
+
+               console.log(nums)
+            
+            
+           
+                 
+
+
+
+
+
+
+
+                
